a80e86e779983b9648e032041ef0e0b864f587f0
hoangxg4 <161178554+hoangxg4@users.noreply.github.com>
2024-04-10 12:38:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-# run.sh
-docker run -d --name adguardhome \
-  -v /data:/opt/adguardhome/data \
-  -p 80:80 \
-  -p 443:443 \
-  -p 53:53/udp \
-  adguardhome
+#!/bin/bash
+/opt/adguardhome/AdGuardHome -c /opt/adguardhome/AdGuardHome.yaml
